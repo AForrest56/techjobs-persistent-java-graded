e75b1321ad435d7ee92c3b965593912130f71aae
Ashley <aforrest56@gmail.com>
2022-11-12 08:23:11 -0600

    Part 3 Add jobs field and updated Job Model

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,8 +1,12 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
@@ -13,6 +17,9 @@ public class Employer extends AbstractEntity {
     @Size(min= 3, message = "Location must be at least 3 characters long")
     private String location;
 
+    @OneToMany
+    @JoinColumn(name="employer_id")
+    private final List<Job> jobs = new ArrayList<>();
 
     public Employer() {}
 
@@ -22,5 +29,9 @@ public class Employer extends AbstractEntity {
 
     public void setLocation(String location) {
         this.location = location;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
     }
 }
